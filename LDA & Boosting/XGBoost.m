c706359a69f17728efb68aece371699a902197f6
Abdullah Rehman <ayr18@ic.ac.uk>
2022-05-19 15:53:06 +0100

    Boosting

---
+++ LDA & Boosting/XGBoost.m
@@ -0,0 +1,105 @@
+close all; clear all;
+
+load monkeydata_training.mat
+
+sigma = 2;
+sz = 10;    % length of gaussFilter vector
+x = linspace(-sz / 2, sz / 2, sz);
+gaussFilter = exp(-x .^ 2 / (2 * sigma ^ 2));
+gaussFilter = gaussFilter / sum (gaussFilter); 
+
+yfilt = conv (trial(1,1).spikes(1,:), gaussFilter, 'same');
+
+firing_rate_trials = cell(100, 8);
+
+for j = 1:8
+    for k = 1:100
+        data_rates = zeros(98, length(trial(k,j).spikes));
+        for i = 1:98
+            data = trial(k,j).spikes(i,:);
+            sigma = 3;
+            sz = 15;    % length of gaussFilter vector
+            x = linspace(-sz / 2, sz / 2, sz);
+            gaussFilter = exp(-x .^ 2 / (2 * sigma ^ 2));
+            gaussFilter = gaussFilter / sum (gaussFilter); % normalize
+            data_rates(i,:) = conv (data, gaussFilter, 'same');
+        end
+        firing_rate_trials{k,j} = data_rates;
+    end
+end
+
+concat_rate_trials = cell(1, 8);
+concat_pos_trials = cell(1, 8);
+
+for j = 1:8
+    for k = 1:100
+        temp_mat = firing_rate_trials{k,j};
+        temp_mat = transpose(temp_mat);
+        
+        temp_pos = trial(k,j).handPos;
+        temp_pos = transpose(temp_pos);
+        
+        if k == 1
+            concat_rate_trials{1,j} = temp_mat;
+            concat_pos_trials{1,j} = temp_pos;
+        else
+            concat_rate_trials{1,j} = [concat_rate_trials{1,j};temp_mat];
+            concat_pos_trials{1,j} = [concat_pos_trials{1,j};temp_pos];
+        end
+    end
+end
+
+pca_rate_trials = cell(1, 8);
+
+for i = 1:8
+    [coeff,score,latent,tsquared,explained,mu] = pca(concat_rate_trials{1,i});
+    strc.coeff = coeff;
+    strc.variance = explained;
+    strc.projection = concat_rate_trials{1,i}*coeff;
+    pca_rate_trials{1,i} = strc;
+end
+
+
+%% boost
+
+tic
+
+
+mdl = fitrensemble(pca_rate_trials{1,1}.projection, concat_pos_trials{1,1}(:,2), 'LearnRate',0.15, 'NumLearningCycles',2000);
+toc
+t = toc;
+%% pred
+tic
+
+pred = predict(mdl, pca_rate_trials{1,1}.projection(1:20000, :));
+
+%score =1 - var(concat_pos_trials{1,1}(1:20000,2)-pred)/var(concat_pos_trials{1,1}(1:20000,2));
+
+rmse = sqrt(abs((mean(sum((pred - concat_pos_trials{1,1}(1:20000,2).*concat_pos_trials{1,1}(1:20000,2))) / sum(concat_pos_trials{1,1}(1:20000,2))))));
+toc
+t=toc;
+
+%% post process
+
+sigma =3;
+sz = 14;    % length of gaussFilter vector
+x = linspace(-sz / 2, sz / 2, sz);
+gaussFilter = exp(-x .^ 2 / (2 * sigma ^ 2));
+gaussFilter = gaussFilter / sum (gaussFilter);
+
+smoothed = conv (pred, gaussFilter, 'same');
+
+figure
+hold on
+grid on
+plot(smoothed, 'linewidth', 1.2)
+plot(concat_pos_trials{1,1}(1:20000,2), 'linewidth', 1.2)
+xlim([1,2000])
+legend('Prediction', 'Ground Truth')
+ylabel('Y Position (mm)')
+xlabel('Time (ms)')
+hold off
+truth = concat_pos_trials{1,1}(1:20000,2);
+
+%% post
+plot(concat_pos_trials{1,1}(:,2))
---
+++ LDA & Boosting/abdu.m
@@ -0,0 +1,71 @@
+spikes = zeros(100,750);
+
+for i=1:100
+    spike = trial(i,1).spikes(1,:);
+    lens = length(spike);
+    spikes(i,1:lens) = spike;   
+end    
+figure(1)    
+imagesc(spikes)
+
+figure(2)
+final_spikes = sum(spikes);
+bar(smooth(final_spikes, 1))
+
+hand_x = trial(71,6).handPos(1,:);
+hand_y = trial(71,6).handPos(2,:);
+
+t = 1:length(hand_x);
+
+figure(3)
+scatter(hand_x, hand_y)
+
+figure(4)
+hold on
+scatter(t, hand_x)
+scatter(t, hand_y)
+hold off
+
+hand_x = trial(4,6).handPos(1,:);
+hand_y = trial(4,6).handPos(2,:);
+
+t = 1:length(hand_x);
+
+figure(5)
+scatter(hand_x, hand_y)
+
+figure(6)
+hold on
+scatter(t, hand_x)
+scatter(t, hand_y)
+hold off
+
+hand_x = trial(78,2).handPos(1,:);
+hand_y = trial(78,2).handPos(2,:);
+
+t = 1:length(hand_x);
+
+figure(7)
+%scatter(hand_x, hand_y)
+plot3(t,hand_x,hand_y)
+xlabel('time')
+
+figure(8)
+hold on
+scatter(t, hand_x)
+scatter(t, hand_y)
+hold off
+
+figure(9)
+vals = zeros(1,8);
+for i=1:8
+    for j=1:100
+        spike = trial(j,i).spikes(1,:);
+        lens = length(spike);
+        spikes(j,1:lens) = spike;
+    end
+    final_spikes = mean(spikes);
+    final_spikes = mean(final_spikes);
+    vals(i) = final_spikes;
+end
+bar(vals)
